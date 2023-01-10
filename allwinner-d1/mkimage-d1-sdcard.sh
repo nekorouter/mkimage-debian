ae5a1f8a7953868dffd576379cc71f2ec1444ead
NekoRouter <nekorouter@outlook.com>
2023-01-11 01:27:55 +0800

    update script

--- allwinner-d1/mkimage-d1-sdcard.sh
+++ allwinner-d1/mkimage-d1-sdcard.sh
@@ -20,12 +20,9 @@ losetup --partscan --find --show "$IMAGE_FILE"
 LOOP_DEVICE=$(losetup -j "$IMAGE_FILE" | grep -o "/dev/loop[0-9]*")
 
 # format partitions
-mkfs.vfat "$LOOP_DEVICE"p1
-mkfs.ext4 "$LOOP_DEVICE"p2
-mkfs.ext4 "$LOOP_DEVICE"p3
-#e2label "$LOOP_DEVICE"p1 efi
-#e2label "$LOOP_DEVICE"p2 boot
-#e2label "$LOOP_DEVICE"p3 root
+mkfs.vfat -F32 -n efi "$LOOP_DEVICE"p1
+mkfs.ext4 -F -L boot "$LOOP_DEVICE"p2
+mkfs.ext4 -F -L root "$LOOP_DEVICE"p3
 
 mkdir "$CHROOT_TARGET"
 mount "$LOOP_DEVICE"p3 "$CHROOT_TARGET"
@@ -56,24 +53,28 @@ chroot "$CHROOT_TARGET" sh -c "locale-gen"
 ###
 
 # Install kernel
-cp linux-image-*.deb "$CHROOT_TARGET"/tmp
-chroot "$CHROOT_TARGET" sh -c "apt install /tmp/*.deb"
+# cp linux-image-*.deb "$CHROOT_TARGET"/tmp
+# chroot "$CHROOT_TARGET" sh -c "apt install /tmp/*.deb"
+cp -v vmlinuz-* "$CHROOT_TARGET"/boot/
+cp -v config-* "$CHROOT_TARGET"/boot/
+# *** should make a "make-kernel.sh" for install kernel form source, because of need of uncompressed kernel ***
 
 # Update package information
-# chroot "$CHROOT_TARGET" sh -c "apt update"
+chroot "$CHROOT_TARGET" sh -c "apt update"
 
 # Install some tools
-chroot "$CHROOT_TARGET" sh -c "apt install -y arch-install-scripts"
+chroot "$CHROOT_TARGET" sh -c "apt install -y initramfs-tools u-boot-menu"
 
 # Change hostname
 chroot "$CHROOT_TARGET" sh -c "echo mqpro > /etc/hostname"
 # chroot "$CHROOT_TARGET" sh -c "echo mqpro > /etc/hosts"
 
 # Set up fstab
-chroot "$CHROOT_TARGET" sh -c "genfstab -U / > /etc/fstab"
+# chroot "$CHROOT_TARGET" sh -c "genfstab -U / > /etc/fstab"
+cp -v fstab "$CHROOT_TARGET"/etc/fstab
 
 # Install kernel and bootloader infrastructure
-chroot "$CHROOT_TARGET" sh -c "apt install -y u-boot-menu"
+# chroot "$CHROOT_TARGET" sh -c "apt install -y u-boot-menu"
 chroot "$CHROOT_TARGET" sh -c "echo 'U_BOOT_ROOT="root=/dev/mmcblk0p3"' | tee -a /etc/default/u-boot"
 chroot "$CHROOT_TARGET" sh -c "u-boot-update"
 
@@ -85,7 +86,7 @@ chroot "$CHROOT_TARGET" sh -c "apt install -y bash-completion network-manager op
 chroot "$CHROOT_TARGET" sh -c "echo 'root:debian' | chpasswd"
 
 # Add new user named "debian"
-chroot "$CHROOT_TARGET" sh -c "useradd -m debian"
+chroot "$CHROOT_TARGET" sh -c "useradd -m debian -s bash"
 chroot "$CHROOT_TARGET" sh -c "echo 'debian:debian' | chpasswd"
 
 # add grub efi and grub.cfg
@@ -93,11 +94,20 @@ mkdir -p "$CHROOT_TARGET"/boot/efi/efi/boot/
 cp -v ../bootriscv64.efi "$CHROOT_TARGET"/boot/efi/efi/boot/
 export kernel_version=$(ls "$CHROOT_TARGET"/boot/ | grep vmlinuz- | sed 's/vmlinuz-//' | head -n 1 )
 cp -v grub.cfg "$CHROOT_TARGET"/boot/
+cp -rv extlinux "$CHROOT_TARGET"/boot/
 sed -i "s/custom_kernel_version/$kernel_version/g" "$CHROOT_TARGET"/boot/grub.cfg
 sed -i "s/custom_kernel_version/$kernel_version/g" "$CHROOT_TARGET"/boot/extlinux/extlinux.conf
 
 # add device tree
 cp -v ./*.dtb "$CHROOT_TARGET"/boot/
+
+# Generate initramfs
+
+if [ x"$(cat "$CHROOT_TARGET"/boot/config-$kernel_version | grep CONFIG_MODULES=y)" = x"CONFIG_MODULES=y" ]; then
+    chroot "$CHROOT_TARGET" /bin/bash -c 'source /etc/profile && update-initramfs -c -k all'
+else
+    sed -i '/initrd/d' "$CHROOT_TARGET"/boot/grub.cfg
+fi
 
 chroot "$CHROOT_TARGET"
 
